5e25a0c550a9048f3906aaa270bcfd27d3e9b197
jennaambruso <90006572+jennaambruso@users.noreply.github.com>
2022-03-21 15:31:30 -0400

    Delete demo directory

--- demo/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/Artboard 1.png", actionRadius: 100, timeBetweenNPCs: 1200, npcCollisionRadius: 100, actionDuration:600, jumpHeight: 300, npcSpeed: 150, maxSimultaneousNPCs: 1, removeNPCAfterInteracted: false, numOfCollectables: 1, removeCollectableAfterCollected: true, postGameScreenImageURLs:"./img/endscreen.png",pointsPerCollectable:1,playUntilScore: 3, pointsPerNPCInteracted: 0});
-});
